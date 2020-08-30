157cde2c34661d2ebe5136ea5b00f06e68c7a1b8
darlanminussi <darlanminussi@gmail.com>
2020-08-29 19:09:22 -0500

    introducing scrappy :poop:

---
+++ R/plotScrappy.R
@@ -0,0 +1,73 @@
+
+#' Make a scrappy plot
+#'
+#' @param object Single cell object. Scrappy recognizes SingleCellExperiment objects or Seurat objects
+#' @param great_qual Percentage of mitochondrial gene expression for a cell to be considered great quality (smaller than)
+#' @param good_qual Percentage of mitochondrial gene expression for a cell to be considered good quality (smaller than)
+#' @param ok_qual Percentage of mitochondrial gene expression for a cell to be considered ok quality (smaller than). Any other percentage above is considered poop quality
+#' @param dimred dimension reductions object to be accessed
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#' @importFrom SummarizedExperiment colData
+#' @importFrom SingleCellExperiment reducedDim
+#' @importFrom Seurat Embeddings
+#' @importFrom ggplot2 ggplot
+#' @importFrom ggimage geom_image
+#' @importFrom dplyr case_when
+plotScrappy <- function(object,
+                        dimred,
+                        great_qual = 5,
+                        good_qual = 10,
+                        ok_qual = 15) {
+
+  ###############################
+  ## SingleCellExperiment wrapper
+  ###############################
+
+  if(class(object) == "SingleCellExperiment") {
+
+    # extract reduced dimension
+    red_dim <- reducedDim(object,
+                          dimred)
+
+    # pull the mitochondrial percentage
+    meta <- colData(object)$subsets_Mito_percent
+
+  }
+
+  ###############################
+  ## Seurat wrapper
+  ###############################
+
+  if (class(object) == "Seurat") {
+
+    meta <- object[["percent.mt"]]$percent.mt
+
+    red_dim <- Embeddings(object,
+                          dimred)
+
+  }
+
+  red_dim <- as.data.frame(red_dim)
+
+  colnames(red_dim) <- c("dim1",
+                         "dim2")
+
+  # classify into emojis
+  red_dim$emoji <- case_when(meta >= 0 & meta <= great_qual ~ "img/smile.png",
+                             meta > great_qual & meta <= good_qual ~ "img/slightly_smile.png",
+                             meta > good_qual & meta <= ok_qual ~ "img/persevering.png",
+                             meta > ok_qual ~ "img/tpoop.png",)
+
+  # plotting
+  ggplot(red_dim, aes(x = dim1,
+                      y = dim2)) +
+    geom_image(aes(image = emoji), size = .03) +
+    theme_classic()
+
+}
+
---
+++ README.md
@@ -0,0 +1,51 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# scrappy
+
+<!-- badges: start -->
+
+<!-- badges: end -->
+
+scrappy provides an easy way to visualize low-quality single-cells.
+
+## Installation
+
+You can install the development version with:
+
+``` r
+install.packages("devtools")
+remotes::install_github("darlanminussi/scrappy")
+```
+
+scrappy uses the percentage of mitochondrial gene expression to classify
+the cells as:
+
+scrappy offers wrappers for
+[SingleCellExperiment](https://osca.bioconductor.org/) as well as
+[Seurat](https://satijalab.org/seurat/) objects.
+
+## Example
+
+``` r
+sce
+#> class: SingleCellExperiment 
+#> dim: 20006 3005 
+#> metadata(0):
+#> assays(2): counts logcounts
+#> rownames(20006): Tspan12 Tshz1 ... mt-Rnr1 mt-Nd4l
+#> rowData names(1): featureType
+#> colnames(3005): 1772071015_C02 1772071017_G12 ... 1772066098_A12
+#>   1772058148_F03
+#> colData names(26): tissue group # ... altexps_repeat_percent total
+#> reducedDimNames(2): PCA TSNE
+#> spikeNames(0):
+#> altExpNames(2): ERCC repeat
+```
+
+``` r
+library(scrappy)
+plotScrappy(sce, "TSNE")
+```
+
+<img src="man/figures/README-example-1.png" width="100%" />
